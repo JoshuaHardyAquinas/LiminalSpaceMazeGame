e36db733c4ca60fac68a4fecd302da741c2fabdc
Joshua Hardy <aq231674@aquinas.ac.uk>
2024-08-04 22:51:30 +0100

    added basic game states, ability to generate new levels in game, ability to gange from 2d representation to 3d rep (3d needs to be coded lol) basic colours to represent menus, mouse is available on menus where buttons would be, game states controll what logic in update runs and controlls what is displayed to scrteen in draw, used switches. uploaded basic font for testing. general comments, made the "maze" global so all functions have access for now,
    TODO:
    make wall entity creation a routine, make raycaster (oh no), code cleanup, naming convention cleanup,

--- LiminalSpaceMazeGame/Game1.cs
+++ LiminalSpaceMazeGame/Game1.cs
@@ -11,18 +11,38 @@ namespace LiminalSpaceMazeGame
     {
         Hero TheHero;
         GenerateMaze TheMaze;
+        SpriteFont GameFont;
 
         List<Wall> walls = new List<Wall>();
 
         private GraphicsDeviceManager _graphics;
         SpriteBatch spriteBatch;
 
-
+        int[,] maze;
         int mazeHieght = 17;
         int mazeWidth = 17;
+        //int mazeHeight;
 
+        //states to switch game between its respective screens
+        enum gamestate
+        {
+            StartMenu,
+            LevelGen,
+            InGame,
+            Dead
+        }
+        //states to swith logic between dimensions
+        enum dimension
+        {
+            D2,
+            D3
+        }
+
+        gamestate currentState = gamestate.StartMenu;
+        dimension Dimension = dimension.D2;
         public Game1()
         {
+            this.IsMouseVisible = true;
             _graphics = new GraphicsDeviceManager(this);
             //change screen size
             _graphics.PreferredBackBufferWidth = 1280;
@@ -38,7 +58,7 @@ namespace LiminalSpaceMazeGame
             // TODO: Add your initialization logic here
 
             //makes 1st maze
-            int[,] maze = TheMaze.GenerateNewMaze(mazeWidth,mazeHieght);
+            maze = TheMaze.GenerateNewMaze(mazeWidth,mazeHieght);
             //creats wall entities to be writen to the screen
             for (int i = 0; i < mazeWidth; i++)
             {
@@ -58,34 +78,81 @@ namespace LiminalSpaceMazeGame
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             TheHero.LoadContent(Content);
-
-            // TODO: use this.Content to load your game content here
+            GameFont = Content.Load<SpriteFont>(@"File");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            KeyboardState ks = Keyboard.GetState();
+            switch (currentState)
+            {
+                case gamestate.StartMenu:
+                    if (ks.IsKeyDown(Keys.Enter))
+                    {
+                        currentState = gamestate.LevelGen;
+                    }
+                    break;
+                case gamestate.LevelGen:
+                    maze = TheMaze.GenerateNewMaze(mazeWidth,mazeHieght);
+                    walls.Clear();
+                    for (int i = 0; i < mazeWidth; i++)
+                    {
+                        for (int j = 0; j < mazeHieght; j++)
+                        {
+                            if (maze[i, j] == 0)
+                            {
+                                Wall newWall = new Wall(i, j);
+                                newWall.LoadContent(Content);
+                                walls.Add(newWall);
+                            }
+                        }
+                    }
+                    if (ks.IsKeyDown(Keys.Enter))
+                    {
+                        currentState = gamestate.InGame;
+                    }
+                    break;
+                case gamestate.InGame:
+                    TheHero.update();
+                    //checks every singe wall for a collision, ineficient but not intensive enough that it causes issues since the 1st check is a collision check
+                    foreach (Wall wall in walls)
+                    {
+                        if (wall.Edge.Intersects(TheHero.Edge))
+                        {
+                            Vector2 centreDis = new Vector2(wall.Edge.Center.X, wall.Edge.Center.Y) - TheHero.Location;//make variable to determine the vector distance away from the center of  the wall in question
+                                                                                                                       //move player away depending on what side is furthur on collision
+                            if (Math.Abs(centreDis.X) > Math.Abs(centreDis.Y))
+                            {
+                                TheHero.Location.X += centreDis.X * -0.125f;
+                            }
+                            else
+                            {
+                                TheHero.Location.Y += centreDis.Y * -0.125f;
+                            }
+                            break;
+                        }
+                    }
+                    if (ks.IsKeyDown(Keys.Enter))
+                    {
+                        currentState = gamestate.Dead;
+                    }
+                    break;
+                case gamestate.Dead:
+                    if (ks.IsKeyDown(Keys.Enter))
+                    {
+                        currentState = gamestate.StartMenu;
+                    }
+                    break;
+                default: 
+                    break;
+            }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
                 Exit();
             }
-            TheHero.update();
-            //checks every singe wall for a collision, ineficient but not intensive enough that it causes issues since the 1st check is a collision check
-            foreach (Wall wall in walls)
+            if (ks.IsKeyDown(Keys.Enter))
             {
-                if (wall.Edge.Intersects(TheHero.Edge))
-                {
-                    Vector2 centreDis = new Vector2(wall.Edge.Center.X, wall.Edge.Center.Y) - TheHero.Location;//make variable to determine the vector distance away from the center of  the wall in question
-                    //move player away depending on what side is furthur on collision
-                    if (Math.Abs(centreDis.X) > Math.Abs(centreDis.Y))
-                    {
-                        TheHero.Location.X += centreDis.X * -0.125f;
-                    }
-                    else
-                    {
-                        TheHero.Location.Y += centreDis.Y * -0.125f;
-                    }
-                    break;
-                }
+                System.Threading.Thread.Sleep(100);
             }
             // TODO: Add your update logic here
 
@@ -95,17 +162,43 @@ namespace LiminalSpaceMazeGame
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            //draw walls below player
-            for (int i = 0; i < walls.Count; i++)
+            switch (currentState)
             {
-                walls[i].draw(spriteBatch);
+                case gamestate.StartMenu:
+                    this.IsMouseVisible = true;
+                    GraphicsDevice.Clear(Color.Yellow);
+                    spriteBatch.DrawString(GameFont, "welcome", new Vector2(0, 0), Color.Black);
+                    break;
+                case gamestate.LevelGen:
+                    GraphicsDevice.Clear(Color.Peru);
+                    this.IsMouseVisible = false;
+                    spriteBatch.DrawString(GameFont, "maze gen", new Vector2(0, 0), Color.Black);
+                    break;
+                case gamestate.InGame:
+                    this.IsMouseVisible = false;
+                    GraphicsDevice.Clear(Color.CornflowerBlue);
+                    //draw walls below player
+                    for (int i = 0; i < walls.Count; i++)
+                    {
+                        walls[i].draw(spriteBatch);
+                    }
+                    //draw hero on top
+                    TheHero.draw(spriteBatch);
+                    break;
+                case gamestate.Dead:
+                    GraphicsDevice.Clear(Color.Red);
+
+                    this.IsMouseVisible = true;
+                    spriteBatch.DrawString(GameFont, "dead", new Vector2(0, 0), Color.Black);
+                    break;
+                default:
+                    GraphicsDevice.Clear(Color.White);
+
+                    spriteBatch.DrawString(GameFont, "error", new Vector2(0, 0), Color.Black);
+                    break;
             }
-            //draw hero on top
-            TheHero.draw(spriteBatch);
             spriteBatch.End();
-            
             base.Draw(gameTime);
         }
     }
